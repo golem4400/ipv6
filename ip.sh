592ea7be842bba431406566883c3bce3318d64ef
golem4400 <88250121+golem4400@users.noreply.github.com>
2022-11-18 11:29:22 +0700

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -45,7 +45,7 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 2000
+maxconn 20000
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -55,9 +55,6 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 stacksize 6291456 
-cache 65536
-nscache 65536
-nscache6 65536
 flush
 auth none
 
